522215a9be28fda447c204904deb2db949099cd4
long <longbao7552@gmail.com>
2024-12-10 17:25:46 -0800

    type

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+const person = {
+    name: '李四',
+    age: 18
+};
+console.log(`我叫${person.name},我今年${person.age}岁`);
+let a;
+let b;
+let c;
+a = 'hello';
+b = -99;
+c = true;
+console.log(a, b, c);
+function count(x, y) {
+    return x + y;
+}
+let result = count(1, 2);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,22 @@
+const person = {
+  name: '李四',
+  age: 18
+}
+console.log(`我叫${person.name},我今年${person.age}岁`)
+
+
+let a: string
+let b: number
+let c: boolean
+
+a = 'hello'
+b = -99
+c = true
+console.log(a, b, c)
+
+function count(x: number, y: number): number {
+  return x + y
+}
+
+let result = count(1, 2)
+console.log(result)
